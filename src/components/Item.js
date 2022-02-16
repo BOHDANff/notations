582fd5e7f4fe7d6999c7fb90a4171d8ccf5f46ab
BOHDANff <popivbod12@gmail.com>
2022-02-17 01:02:47 +0200

    Adding warning modal before deleting of notation

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import MyButton from "./UI/MyButton/MyButton";
 import {useDispatch} from "react-redux";
 import {deleteItem} from "../store/reducers/ListReducer";
+import MyModal from "./UI/MyModal/MyModal";
 
 const Item = (props) => {
     const dispatch = useDispatch()
+    const [deleteVisible, setDeleteVisible] = useState(false)
     return (
         <div className="item">
             <div className="item__content">
@@ -17,9 +19,16 @@ const Item = (props) => {
                 <MyButton style={{width: "100%", marginBottom: "10px"}}>
                     Open
                 </MyButton>
-                <MyButton style={{width: "100%"}} onClick={() => dispatch(deleteItem(props.id))}>
+                <MyButton style={{width: "100%"}} onClick={() => setDeleteVisible(true)}>
                     Delete
                 </MyButton>
+                <MyModal visible={deleteVisible} setVisible={setDeleteVisible}>
+                    <div>Are you realy want to delete this notation?</div>
+                    <MyButton onClick={() => {
+                        dispatch(deleteItem(props.id))
+                        setDeleteVisible(false)
+                    }}>Delete</MyButton>
+                </MyModal>
             </div>
         </div>
     );
